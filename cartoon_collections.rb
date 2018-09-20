1a7480f59e45c125fc72630320d684766cdbd81b
Veniel Dorestave <veniel@noukod.com>
2018-09-20 16:32:26 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,4 +1,4 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your codwarves.each_with_index do |name, index|
   dwarves.each_with_index do |name, index|
    index += 1
@@ -6,21 +6,21 @@ def roll_call_dwarves# code an argument here
 end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
   planeteer_calls.collect do |call|
     call.capitalize << "!"
 end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)# code an argument here
   # Your code here
   calls.any? do |call|
     call.length > 4
 end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   i = 0
     cheese_types = ["cheddar", "gouda", "camembert"]
